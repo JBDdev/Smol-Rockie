0cedf1ccd261e3d3143fdde81795a834322a050b
Kat <panguino729@gmail.com>
2022-02-24 14:22:54 -0500

    Added code to the UI scripts; added all of the new scenes to the build settings

--- 603Game4/Assets/Scripts/UI/ButtonManager.cs
+++ 603Game4/Assets/Scripts/UI/ButtonManager.cs
@@ -1,18 +1,53 @@
+// Code source: Katarina Tretter
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject uI;
+
+    public void OnStart()
     {
-        
+        Debug.Log("Clicked Start");
+        Time.timeScale = 1;
+        SceneManager.LoadScene("GameScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnResume()
     {
-        
+        Debug.Log("Clicked Resume");
+        uI.GetComponent<UIManager>().ButtonPress("resume");
+    }
+
+    public void OnOptions()
+    {
+        Debug.Log("Clicked Options");
+        uI.GetComponent<UIManager>().ButtonPress("options");
+    }
+
+    public void OnBack()
+    {
+        Debug.Log("Clicked Back");
+        uI.GetComponent<UIManager>().ButtonPress("back");
+    }
+
+    public void OnCreedits()
+    {
+        Debug.Log("Clicked Credits");
+    }
+
+    public void OnMainMenu()
+    {
+        Debug.Log("Clicked Main Menu");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OnQuit()
+    {
+        Debug.Log("Clicked Quit");
+        Application.Quit();
     }
 }
