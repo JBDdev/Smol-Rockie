190da2d130964de558bc5a01375abe1a426b80df
Brody Davison (RIT Student) <brodydavison4@gmail.com>
2022-02-24 15:17:55 -0500

    Script setup for data storage
    Rocks probably won't need an external file, but will be nicer for cosmetics potentially

---
+++ 603Game4/Assets/Scripts/Data Management/RockData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RockData : MonoBehaviour
+{
+
+    public string[] rockNames;
+    public GameObject[] rockPrefabs;
+    //public variable for 2D sprite (not sure what data type yet, based on UI implementation)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
