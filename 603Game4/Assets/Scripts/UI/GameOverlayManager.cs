8464006c17fc7efe05cd4206bd250e4dc656d0c4
Katarina Tretter <panguino729@gmail.com>
2022-02-25 12:37:46 -0500

    Added currency overlay

--- 603Game4/Assets/Scripts/UI/GameOverlayManager.cs
+++ 603Game4/Assets/Scripts/UI/GameOverlayManager.cs
@@ -5,10 +5,17 @@ using UnityEngine.UI;
 
 public class GameOverlayManager : MonoBehaviour
 {
+    [SerializeField] private GameObject gameManager;
+    [SerializeField] private Text rCHNumber;
+    [SerializeField] private Text rCNumber;
+    [SerializeField] private Text pNumber;
+
+    private GameManager gM;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gM = gameManager.GetComponent<GameManager>();
     }
 
     // Update is called once per frame
