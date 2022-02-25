f99d44dec3be266b173e5ae47f1cf8f881ba1c23
Katarina Tretter <panguino729@gmail.com>
2022-02-25 14:07:49 -0500

    Got screenshots to work (still have UI visible)
    The screenshots will save a .png to .\Documents\Smol Rockie\Screenshots (it will also create that folder structure if it does not already exist)

--- 603Game4/Assets/Scripts/UI/Screenshot.cs
+++ 603Game4/Assets/Scripts/UI/Screenshot.cs
@@ -1,4 +1,5 @@
 // Code provided by: PaulaOstupe - https://answers.unity.com/questions/1498087/capture-screenshots-and-save-them-in-folder.html
+// Microsoft Documentation - https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-create-a-file-or-folder
 
 using System;
 using System.IO;
@@ -11,11 +12,35 @@ public class Screenshot : MonoBehaviour
 {
     [SerializeField] GameObject uI;
 
+    private String gamePath;
+    private String screenshotsPath;
+
     public void onScreenshot()
     {
         uI.GetComponent<UIManager>().ButtonPress("cameraStart"); // Doesn't work
-        Debug.Log("Clicked Screenshot: " + Application.dataPath + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss" + ".png"));
-        ScreenCapture.CaptureScreenshot(Application.dataPath + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".png");
+
+        // Set paths
+        gamePath = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + Path.DirectorySeparatorChar + "Smol Rockie";
+        screenshotsPath = gamePath + Path.DirectorySeparatorChar + "Screenshots";
+
+        // Check if either folder exists already
+        if (!Directory.Exists(gamePath))
+        {
+            if (!Directory.Exists(screenshotsPath))
+            {
+                // If not, create
+                Directory.CreateDirectory(gamePath);
+                Directory.CreateDirectory(screenshotsPath);
+
+            }
+        }
+        // IF exists, take screenshot
+        else
+        {
+            Debug.Log(screenshotsPath + Path.DirectorySeparatorChar + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".png");
+            ScreenCapture.CaptureScreenshot(screenshotsPath + Path.DirectorySeparatorChar + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".png");
+        }
+
         uI.GetComponent<UIManager>().ButtonPress("cameraEnd");
     }
 }
