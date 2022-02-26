78ada65f768a41d081b096e497740840c54dd103
JBDdev <brodydavison4@gmail.com>
2022-02-26 15:48:04 -0500

    File read + write, collection data
    Implemented collection framework. Supports reading and writing from playerdata.txt

--- 603Game4/Assets/Scripts/Data Management/Collection.cs
+++ 603Game4/Assets/Scripts/Data Management/Collection.cs
@@ -1,14 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-[System.Serializable]
+
 public class Collection : MonoBehaviour
 {
+    public CollectionData data;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!File.Exists("./playerdata.txt"))
+        {
+
+            data = new CollectionData(gameObject.GetComponent<RockData>().NumRocks, gameObject.GetComponent<CosmeticsData>().NumFourStars, gameObject.GetComponent<CosmeticsData>().NumFiveStars);
+            string output = JsonUtility.ToJson(data, true);
+
+            //Write values to file
+            FileStream stream = new FileStream("./playerdata.txt", FileMode.OpenOrCreate);
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                writer.WriteLine(output);
+            }
+        }
+        else
+        {
+            string output;
+            using (StreamReader reader = new StreamReader("./playerdata.txt")) 
+            {
+                output = reader.ReadToEnd();
+            }
+               
+            data = JsonUtility.FromJson<CollectionData>(output);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+            UpdateFile();
+    }
+
+    // This function should be called whenever updates to the player's collection (i.e. gained currency, new item from gacha pull, etc.)
+    public void UpdateFile()
+    {
+        string output = JsonUtility.ToJson(data, true);
+
+        //Write values to file
+        FileStream stream = new FileStream("./playerdata.txt", FileMode.Create);
+        using (StreamWriter writer = new StreamWriter(stream))
+        {
+            writer.WriteLine(output);
+        }
+    }
+
+    
+}
+
+//Data was split into a separate object to make use of Unity's JSON Serialization Utility
+[System.Serializable]
+public class CollectionData
+{
+    public CollectionData(int numRocks, int numFourStars, int numFiveStars) 
+    {
+        //Default Collection values
+        collectedRocks = new bool[numRocks];
+        fourStarCos = new bool[numFourStars];
+        fiveStarCos = new bool[numFiveStars];
+        currentRock = -1;
+        hat = -1;
+        face = -1;
+        neck = -1;
+    }
+
+    public CollectionData() 
+    {
+    
+    }
     //Index values for collected items
-    public int[] collectedRocks;
-    public int[] fourStarCos;
-    public int[] fiveStarCos;
+    public bool[] collectedRocks;
+    public bool[] fourStarCos;
+    public bool[] fiveStarCos;
 
     [Header("Current Pet Rock")]
     public int currentRock;
@@ -19,29 +92,4 @@ public class Collection : MonoBehaviour
     [Header("Resources")]
     public int money;
     public int premiumMoney;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Psuedocode hellscape begins here (Determine file location where the data will be stored)
-
-        /* Check to see if collection file exists 
-         * 
-         * if doesn't exist: create the file. Setup default values for collection, then write data to file.
-         *
-         * if exists, take the json data and convert it into vars for this obj.
-         */
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    // This function should be called whenever updates to the player's collection (i.e. gained currency, new item from gacha pull, etc.)
-    public void UpdateFile() 
-    {
-        //Do a ToJson on this object (w/ pretty print) and write it to the collection.json file
-    }
 }
