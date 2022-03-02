5056695056fb9efd34d802e50d4177376c8bab65
Katarina Tretter <panguino729@gmail.com>
2022-03-02 15:28:40 -0500

    Added scripts back

---
+++ 603Game4/Assets/Scripts/UI/InventorySlot.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    [SerializeField] private Image itemIcon;
+    [SerializeField] private string itemName;
+    [SerializeField] private Sprite itemImage;
+
+    public void AddItem(string name, Sprite image)
+    {
+        itemIcon.sprite = image; ;
+    }
+}
